f5064e56008f65de54b404cac7fc26d1600f5f92
kheil <kheilman@nd.edu>
2015-04-01 15:56:12 -0400

    Added resampling to make sure soil rasters have the same resolution and grid as the statistical model output.

--- R/surgo_soil_data_processing.R
+++ R/surgo_soil_data_processing.R
@@ -1,67 +1,81 @@
 #This code reads the .asc soil rasters into R as rasters, plots them, and converts them to dataframes
 library(raster)
-
+library(sp)
 #see README.txt for descriptions of the data and citation for STATSGO2 soil data
-
 
 #simons code to create the 8km albers grid that I used to get the soil data on the 
 #same grid scale
-input <- raster('paleon.unit.alb_01.tif')
+input <- raster('data/Abies_balsamea_diam_alb.tif') #this file came from biomass dropbox
 input <- setValues(input, 1:ncell(input))
-writeRaster(input, "paleon8kmalbers.tif")
+writeRaster(input, "paleon8km_midalbers1.tif")
 
-
+input.df<-as.data.frame(input, xy=TRUE)
 #setwd('location of your soil directory')
 
-#read the soil rasters into R
-#example for ksat
-ksat<-raster("paleon_ksat.asc")
+
+##read the soil rasters into R
+##example for ksat
+ksat<-raster("Data/paleon_ksat.asc")
 #define Albers projcetion for this layer
 proj4string(ksat)<-CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
-plot(ksat)
-#convert this raster to a dataframe to work with the data
-#adding xy=TRUE outputs the x-y albers coordinates with the data value
-ksat.df<-as.data.frame(ksat, xy=TRUE)
+plot(ksat) #note that the extent of these data goes beyond statistical output
+ksat <- resample(ksat, input) #resample ksat based on the psleon8km_midalbers1 grid created above
 
-#Repeat this process for the rest of the soil characters
-#for AWC
-awc<-raster("paleon_awc.asc")
-proj4string(awc)<-CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
+#need to convert this raster to a dataframe to work with the data
+
+ksat.df <- as.data.frame(ksat, xy=TRUE) #adding xy=TRUE outputs the x-y albers coordinates with the data value
+
+
+
+##Repeat this process for the rest of the soil characters
+##for AWC
+awc <- raster("Data/paleon_awc.asc")
+proj4string(awc) <- CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
 plot(awc)
-awc.df<-as.data.frame(awc, xy=TRUE)
+awc <- resample(awc, input)
+awc.df <- as.data.frame(awc, xy=TRUE)
 
-#for sand
-sand<-raster("paleon_sand.asc")
-proj4string(sand)<-CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
+##for sand
+sand <- raster("Data/paleon_sand.asc")
+proj4string(sand) <- CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
 plot(sand)
-sand.df<-as.data.frame(sand, xy=TRUE)
+sand <- resample(sand, input)
+sand.df <- as.data.frame(sand, xy=TRUE)
 
-#for silt
-silt<-raster("paleon_silt.asc")
-proj4string(silt)<-CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
+##for silt
+silt <- raster("Data/paleon_silt.asc")
+proj4string(silt) <- CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
 plot(silt)
-silt.df<-as.data.frame(silt, xy=TRUE)
+silt <- resample(silt, input)
+silt.df <- as.data.frame(silt, xy=TRUE)
 
-#for clay
-clay<-raster("paleon_clay.asc")
-proj4string(clay)<-CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
+##for clay
+clay <- raster("Data/paleon_clay.asc")
+proj4string(clay) <- CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
 plot(clay)
-clay.df<-as.data.frame(clay, xy=TRUE)
+clay <- resample(clay, input)
+clay.df <- as.data.frame(clay, xy=TRUE)
 
 #for elev from statsgo
-elev<-raster("paleon_elev.asc")
-proj4string(elev)<-CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
+elev<-raster("Data/paleon_elev.asc")
+proj4string(elev) <- CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
 plot(elev)
-elev.cell<-extract(elev,extent(elev))
-elev.df<-as.data.frame(elev, xy=TRUE)
+elev <-resample(elev, input)
+elev.cell <- extract(elev,extent(elev))
+elev.df <- as.data.frame(elev, xy=TRUE)
 
 
-#read in biomass data
-biomass<-read.csv('plss_biomass_v0.9-1.csv')#or gridded total biomass
+##this biomass data does not have the same x and y values as the statistical model
+#therefore, this biomass grid does not match up when I try to merge the biomass data with the soil data
+biomass <- read.csv('Data/plss_biomass_alb_v0.9-3.csv')#this is gridded non-biomass model biomass
+biomass.df<-data.frame(biomass)
+coordinates(biomass) <- ~x+y
+biomass <- raster(biomass)
+values(biomass)<-biomass
 
-coordinates(biomass)<-~x+y
-biomass<-raster(biomass)
-proj4string(biomass)<-CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
+proj4string(biomass) <- CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
 
+
+sample.merged.data<-merge(biomass.df, ksat.df, by =c("x", "y"))
 
 

bb0c66d17b1d44ad132c1afe6cbef5e6cabc8406
kheil <kheilman@nd.edu>
2015-07-13 13:16:48 -0400

    changed "input" base raster to the full paleon domain for consistancy

--- R/surgo_soil_data_processing.R
+++ R/surgo_soil_data_processing.R
@@ -5,7 +5,7 @@ library(sp)
 
 #simons code to create the 8km albers grid that I used to get the soil data on the 
 #same grid scale
-input <- raster('data/Abies_balsamea_diam_alb.tif') #this file came from biomass dropbox
+input <- raster('Data/paleon.unit.alb_01.tif') #this file came from biomass dropbox
 input <- setValues(input, 1:ncell(input))
 writeRaster(input, "paleon8km_midalbers1.tif", overwrite=TRUE)
 
@@ -15,9 +15,10 @@ input.df <- as.data.frame(input, xy=TRUE)
 
 ##read the soil rasters into R
 ##example ksat
-ksat <- raster("Data/paleon_ksat.asc")
+ksat <- raster("Data/ksat_raster1.tif")
 #define Albers projcetion for this layer
-proj4string(ksat) <- CRS('+init=epsg:3175')
+ksat<-projectRaster(ksat, crs='+init=epsg:3175', method='bilinear')
+
 plot(ksat) #note that the extent of these data goes beyond statistical output
 
 ##use resample to realign if the cells are off by a small amount
@@ -33,26 +34,25 @@ write.csv(ksat2.df, "ksat.soil.csv") #write to csv so you can haave it by itself
 #Rest of the soil data
 ##########################
 #clay
-clay <- raster("Data/paleon_clay.asc")
-#define Albers projcetion for this layer
-proj4string(clay) <- CRS('+init=epsg:3175')
+clay <- raster("Data/clay_rast1.tif")
+#project into Albers projcetion for this layer
+clay<-projectRaster(clay, crs='+init=epsg:3175', method='bilinear')
+
 plot(clay) #note that the extent of these data goes beyond statistical output
 clay3 <- resample(clay, input, method='ngb') #resample ksat based on the psleon8km_midalbers1 grid created above
-clay2 <- stack(clay3, input) #ksat has values of soil parameter, while input has values of "cell number of interest"
-#ksat$cell <- setValues(ksat, 1:ncell(ksat)) #not sure if this is a valid thing to do, but this creates RasterBrick
-
-##need to convert this raster to a dataframe to work with the data
+# to convert this raster to a dataframe to work with the data
 clay3.df <- as.data.frame(clay3, xy=TRUE, cellnumbers=TRUE) #adding xy=TRUE outputs the x-y albers coordinates with the data value
 write.csv(clay3.df, "clay.soil.csv") 
 
 ########################
 #sand
 #######################
-sand <- raster("Data/paleon_sand.asc")
+sand <- raster("Data/sand_rast1.tif")
 #define Albers projcetion for this layer
-proj4string(sand) <- CRS('+init=epsg:3175')
+sand<-projectRaster(sand, crs='+init=epsg:3175', method='bilinear')
+
 plot(sand) #note that the extent of these data goes beyond statistical output
-sand3 <- resample(sand, input, method='ngb') #resample ksat based on the psleon8km_midalbers1 grid created above
+sand3 <- resample(sand, input, method='bilinear') #resample ksat based on the psleon8km_midalbers1 grid created above
 sand2 <- stack(sand3, input) #ksat has values of soil parameter, while input has values of "cell number of interest"
 #ksat$cell <- setValues(ksat, 1:ncell(ksat)) #not sure if this is a valid thing to do, but this creates RasterBrick
 
@@ -63,13 +63,13 @@ write.csv(sand3.df, "sand.soil.csv")
 #####################
 #silt
 #####################
-silt <- raster("Data/paleon_silt.asc")
+silt <- raster("Data/silt_rast1.tif")
 #define Albers projcetion for this layer
-proj4string(silt) <- CRS('+init=epsg:3175')
+silt<-projectRaster(silt, crs='+init=epsg:3175', method='bilinear')
+
 plot(silt) #note that the extent of these data goes beyond statistical output
-silt3 <- resample(silt, input, method='ngb') #resample ksat based on the psleon8km_midalbers1 grid created above
+silt3 <- resample(silt, input, method='bilinear') #resample ksat based on the psleon8km_midalbers1 grid created above
 silt2 <- stack(silt3, input) #ksat has values of soil parameter, while input has values of "cell number of interest"
-#ksat$cell <- setValues(ksat, 1:ncell(ksat)) #not sure if this is a valid thing to do, but this creates RasterBrick
 
 ##need to convert this raster to a dataframe to work with the data
 silt3.df <- as.data.frame(silt3, xy=TRUE, cellnumbers=TRUE) #adding xy=TRUE outputs the x-y albers coordinates with the data value
@@ -78,9 +78,11 @@ write.csv(silt3.df, "silt.soil.csv")
 #######################
 #elev
 #######################
-elev <- raster("Data/paleon_elev.asc")
+elev <- raster("Data/elev_rast1.tif")
 #define Albers projcetion for this layer
-proj4string(elev) <- CRS('+init=epsg:3175')
+elev<-projectRaster(elev, crs='+init=epsg:3175', method='bilinear')
+
+
 plot(elev) #note that the extent of these data goes beyond statistical output
 elev3 <- resample(elev, input, method='ngb') #resample ksat based on the psleon8km_midalbers1 grid created above
 elev2 <- stack(elev3, input) #ksat has values of soil parameter, while input has values of "cell number of interest"
@@ -88,17 +90,17 @@ elev2 <- stack(elev3, input) #ksat has values of soil parameter, while input has
 
 ##need to convert this raster to a dataframe to work with the data
 elev3.df <- as.data.frame(elev3, xy=TRUE, cellnumbers=TRUE) #adding xy=TRUE outputs the x-y albers coordinates with the data value
-write.csv(elev3.df, "clay.soil.csv")
+write.csv(elev3.df, "elev.soil.csv")
 ######################
 #awc
 #####################
-awc <- raster("Data/paleon_awc.asc")
+awc <- raster("Data/awc_rast1.tif")
 #define Albers projcetion for this layer
-proj4string(awc) <- CRS('+init=epsg:3175')
+awc<-projectRaster(awc, crs='+init=epsg:3175', method='bilinear')
+
 plot(awc) #note that the extent of these data goes beyond statistical output
-awc3 <- resample(awc, ksat, method='ngb') #resample ksat based on the psleon8km_midalbers1 grid created above
+awc3 <- resample(awc, input, method='ngb') #resample ksat based on the psleon8km_midalbers1 grid created above
 awc2 <- stack(awc3, input) #ksat has values of soil parameter, while input has values of "cell number of interest"
-#ksat$cell <- setValues(ksat, 1:ncell(ksat)) #not sure if this is a valid thing to do, but this creates RasterBrick
 
 ##need to convert this raster to a dataframe to work with the data
 awc3.df <- as.data.frame(awc3, xy=TRUE, cellnumbers=TRUE) #adding xy=TRUE outputs the x-y albers coordinates with the data value
@@ -108,13 +110,25 @@ write.csv(awc3.df, "awc.soil.csv")
 ###################################
 #Add soil to the biomass data grid#
 ###################################
-biomass <- read.csv('Data/biomass_v09_extract (2).csv')#this is gridded non-biomass model biomass
-biomass$total <- rowSums(biomass[,5:32], na.rm = TRUE)
+#read in the upper midwest data
+biomass <- read.csv('Data/biomass_prediction_v0.2-1.csv')#this is gridded non-biomass model biomass
+biomass$total <- rowSums(biomass[,4:25], na.rm = TRUE)
 coordinates(biomass) <- ~x+y #assign coordinates to make biomass spatial points datframe
 proj4string(biomass) <- CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
+
+#read in the indiana and illinois data
+biomass.inil<- read.csv('C:/Users/Kelly/Documents/Indiana_Density_Biomass/Data/outputsplss_biomass_indiana_illinois_v1.csv')
+biomass.inil$total <- rowSums(biomass.inil[,5:36], na.rm = TRUE)
+coordinates(biomass.inil) <- ~x+y #assign coordinates to make biomass spatial points datframe
+proj4string(biomass.inil) <- CRS('+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
+
+#merge the biomass rasters
+test <- merge(biomass, biomass.inil, overlap=FALSE)
 rast<-awc
 Oak.biomass<- rasterize(biomass, rast, field = "Oak")
 tot.biomass <- rasterize(biomass, rast, field = "total")
+Oak.biomass.il<-rasterize(biomass.inil, rast, field="Oak")
+tot.biomass.il <- rasterize(biomass.inil, rast, field="total")
 biomass.tot<- biomass$total
 
 
@@ -165,7 +179,7 @@ dev.off()
 
 
 biomass.df <- data.frame(biomass) #export the data as a dataframe
-
+biomass.ilin.df<- data.frame(biomass.inil)
 #make a dataframe with only the soil data
 soils.df <- biomass.df
 soils<- c("x", "y", "sand", "silt", "clay", "awc", "ksat", "elev")
